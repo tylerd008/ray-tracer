b9fa02e32636e195c2f8240e58379c4944a6a234
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-19 22:02:22 -0400

    Add progress indication to loop

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("P3\n{} {}\n 255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
             let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
