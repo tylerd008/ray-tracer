d0255da4c36a312b618d7a50f5f24c67a20c5942
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-20 11:54:44 -0400

    Generate blue to white y gradient

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 use ray_tracer::color::*;
+use ray_tracer::ray::*;
+use ray_tracer::vec3::*;
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = Vec3::unit_vector(r.direction);
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
-    const IMAGE_WIDTH: usize = 256;
-    const IMAGE_HEIGHT: usize = 256;
+    //image dimensions
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: usize = 400;
+    const IMAGE_HEIGHT: usize = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as usize;
+
+    //camera setup
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3\n{} {}\n 255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (j as f64) / ((IMAGE_WIDTH - 1) as f64),
-                0.25,
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(r);
             write_color(pixel_color);
         }
     }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,45 +1,48 @@
 use std::fmt;
 use std::ops;
 
+#[derive(Clone, Copy)]
 pub struct Vec3 {
-    e: Vec<f64>,
+    e0: f64,
+    e1: f64,
+    e2: f64,
 }
 
 impl Vec3 {
     pub fn new_empty() -> Self {
-        Self { e: vec![0.0; 3] }
-    }
-    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
         Self {
-            e: vec![e0, e1, e2],
+            e0: 0.0,
+            e1: 0.0,
+            e2: 0.0,
         }
     }
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        Self { e0, e1, e2 }
+    }
     pub fn x(&self) -> f64 {
-        self.e[0]
+        self.e0
     }
     pub fn y(&self) -> f64 {
-        self.e[1]
+        self.e1
     }
     pub fn z(&self) -> f64 {
-        self.e[2]
+        self.e2
     }
     pub fn length_squared(&self) -> f64 {
-        (self.e[0] * self.e[0]) + (self.e[1] * self.e[1]) + (self.e[2] * self.e[2])
+        (self.e0 * self.e0) + (self.e1 * self.e1) + (self.e2 * self.e2)
     }
 
     pub fn length(&self) -> f64 {
         self.length_squared().powf(0.5)
     }
     pub fn dot(v: Vec3, u: Vec3) -> f64 {
-        (v.e[0] * u.e[0]) + (v.e[1] * u.e[1]) + (v.e[2] * u.e[2])
+        (v.e0 * u.e0) + (v.e1 * u.e1) + (v.e2 * u.e2)
     }
     pub fn cross(v: Vec3, u: Vec3) -> Vec3 {
         Self {
-            e: vec![
-                v.e[1] * u.e[2] - v.e[2] * u.e[1],
-                v.e[2] * u.e[0] - v.e[0] * u.e[2],
-                v.e[0] * u.e[1] - v.e[1] * u.e[0],
-            ],
+            e0: v.e1 * u.e2 - v.e2 * u.e1,
+            e1: v.e2 * u.e0 - v.e0 * u.e2,
+            e2: v.e0 * u.e1 - v.e1 * u.e0,
         }
     }
     pub fn unit_vector(v: Vec3) -> Vec3 {
@@ -50,17 +53,17 @@ impl Vec3 {
 
 impl ops::AddAssign<Vec3> for Vec3 {
     fn add_assign(&mut self, rhs: Self) {
-        for i in 0..3 {
-            self.e[i] *= rhs.e[i];
-        }
+        self.e0 *= rhs.e0;
+        self.e1 *= rhs.e1;
+        self.e2 *= rhs.e2;
     }
 }
 
 impl ops::MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, rhs: f64) {
-        for i in 0..3 {
-            self.e[i] *= rhs;
-        }
+        self.e0 *= rhs;
+        self.e1 *= rhs;
+        self.e2 *= rhs;
     }
 }
 
@@ -73,9 +76,9 @@ impl ops::DivAssign<f64> for Vec3 {
 impl ops::Neg for Vec3 {
     type Output = Vec3;
     fn neg(mut self) -> Self::Output {
-        for i in 0..3 {
-            self.e[i] *= -1.0;
-        }
+        self.e0 *= -1.0;
+        self.e1 *= -1.0;
+        self.e2 *= -1.0;
         self
     }
 }
@@ -90,11 +93,9 @@ impl ops::Add<Vec3> for Vec3 {
     type Output = Vec3;
     fn add(self, rhs: Vec3) -> Self::Output {
         Self {
-            e: vec![
-                self.e[0] + rhs.e[0],
-                self.e[1] + rhs.e[1],
-                self.e[2] + rhs.e[2],
-            ],
+            e0: self.e0 + rhs.e0,
+            e1: self.e1 + rhs.e1,
+            e2: self.e2 + rhs.e2,
         }
     }
 }
@@ -102,11 +103,9 @@ impl ops::Sub<Vec3> for Vec3 {
     type Output = Vec3;
     fn sub(self, rhs: Vec3) -> Self::Output {
         Self {
-            e: vec![
-                self.e[0] - rhs.e[0],
-                self.e[1] - rhs.e[1],
-                self.e[2] - rhs.e[2],
-            ],
+            e0: self.e0 - rhs.e0,
+            e1: self.e1 - rhs.e1,
+            e2: self.e2 - rhs.e2,
         }
     }
 }
@@ -115,11 +114,9 @@ impl ops::Mul<Vec3> for Vec3 {
     type Output = Vec3;
     fn mul(self, rhs: Vec3) -> Self::Output {
         Self {
-            e: vec![
-                self.e[0] * rhs.e[0],
-                self.e[1] * rhs.e[1],
-                self.e[2] * rhs.e[2],
-            ],
+            e0: self.e0 * rhs.e0,
+            e1: self.e1 * rhs.e1,
+            e2: self.e2 * rhs.e2,
         }
     }
 }
@@ -128,7 +125,9 @@ impl ops::Mul<f64> for Vec3 {
     type Output = Vec3;
     fn mul(self, rhs: f64) -> Self::Output {
         Self {
-            e: vec![self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs],
+            e0: self.e0 * rhs,
+            e1: self.e1 * rhs,
+            e2: self.e2 * rhs,
         }
     }
 }
