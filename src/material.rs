d1975a226407f351bf5df2f3f80edf7ffcb8fa1a
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-25 17:49:00 -0400

    Implement fuzz for `Metal`

--- src/material.rs
+++ src/material.rs
@@ -23,7 +23,7 @@ impl Lambertian {
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<ScatterData> {
+    fn scatter(&self, _r_in: &Ray, rec: &HitRecord) -> Option<ScatterData> {
         let mut scatter_direction = rec.normal + Vec3::random_unit_vector(); //feel like i should be able to write this without needing it to be mut
 
         if scatter_direction.near_zero() {
@@ -39,18 +39,20 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        let fuzz = if fuzz < 1.0 { fuzz } else { 1.0 };
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<ScatterData> {
         let reflected = Vec3::reflect(Vec3::unit_vector(r_in.direction), rec.normal);
-        let scattered = Ray::new(rec.p, reflected);
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
 
         if Vec3::dot(scattered.direction, rec.normal) > 0.0 {
             Some(ScatterData {
