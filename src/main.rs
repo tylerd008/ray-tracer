e116488a019b6da0f7deef09de1610423e1a6c59
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-26 16:26:32 -0400

    Final scene for first book

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use ray_tracer::camera::Camera;
 use ray_tracer::color::*;
 use ray_tracer::hittable::{HitRecord, Hittable};
 use ray_tracer::hittable_list::HittableList;
-use ray_tracer::material::{Dielectric, Lambertian, Metal};
+use ray_tracer::material::{Dielectric, Lambertian, Material, Metal};
 use ray_tracer::ray::*;
 use ray_tracer::sphere::Sphere;
 use ray_tracer::vec3::*;
@@ -26,55 +26,84 @@ fn ray_color<T: Hittable>(r: Ray, world: &T, depth: usize) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
+fn rand_f64() -> f64 {
+    let mut rng = thread_rng();
+    rng.gen_range(0.0..1.0)
+}
+
+fn rand_f64_range(min: f64, max: f64) -> f64 {
+    let mut rng = thread_rng();
+    rng.gen_range(min..max)
+}
+
+fn random_scene() -> HittableList {
+    let mut world = HittableList::new();
+    let ground_mat = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_mat,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand_f64();
+            let center = Point3::new(
+                a as f64 + 0.9 * rand_f64(),
+                0.2,
+                b as f64 + 0.9 * rand_f64(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_mat: Rc<dyn Material>;
+                if choose_mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    sphere_mat = Rc::new(Lambertian::new(albedo));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_mat)));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = rand_f64_range(0.0, 0.5);
+                    sphere_mat = Rc::new(Metal::new(albedo, fuzz));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_mat)));
+                } else {
+                    sphere_mat = Rc::new(Dielectric::new(1.5));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_mat)));
+                }
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.5));
+    world.add(Rc::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat1)));
+
+    let mat2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Rc::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat2)));
+
+    let mat3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Rc::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat3)));
+
+    world
+}
+
 fn main() {
     //image dimensions
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: usize = 400;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const IMAGE_WIDTH: usize = 1200;
     const IMAGE_HEIGHT: usize = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as usize;
-    const SAMPLES_PER_PIXEL: usize = 100;
+    const SAMPLES_PER_PIXEL: usize = 500;
     const MAX_DEPTH: usize = 50;
 
     //world setup
 
-    let mut world = HittableList::new();
-    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = Rc::new(Dielectric::new(1.5));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
-
-    world.add(Rc::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        mat_ground,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.0),
-        0.5,
-        mat_center,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        mat_left,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        -0.4,
-        Rc::new(Dielectric::new(1.5)),
-    )));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        mat_right,
-    )));
+    let world = random_scene();
 
     //camera setup
 
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_at = Point3::new(0.0, 0.0, -1.0);
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_at = Point3::new(0.0, 0.0, 0.0);
     let v_up = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (look_from - look_at).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
     let cam = Camera::new(
         look_from,
         look_at,
